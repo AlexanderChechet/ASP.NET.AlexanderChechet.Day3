586eb05b1c1e0d90c5ef1a2bff27abd7dd071c68
AlexanderChechet <Chechetsasha95@gmail.com>
2015-07-16 02:23:13 +0300

    Day3Commit1

---
+++ Polynom/CustomPolynom.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Text;
+
+namespace Polynom
+{
+    public class CustomPolynom : IEquatable<CustomPolynom>, ICloneable
+    {
+        #region private members
+        private double?[] polynom;
+        #endregion
+
+        #region Ctors
+        public CustomPolynom(CustomPolynom customPolynom)
+        {
+            MaxPower = customPolynom.MaxPower;
+            polynom = new double?[MaxPower];
+            for (int i = 0; i < MaxPower; i++)
+            {
+                polynom[i] = customPolynom[i];
+            }
+        }
+
+        public CustomPolynom(params double?[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            MaxPower = array.Length;
+            polynom = new double?[MaxPower];
+            array.CopyTo(polynom, 0);
+        }
+        #endregion
+
+        #region Static methods
+        public static CustomPolynom Add(CustomPolynom left, CustomPolynom right)
+        {
+            int max = Math.Max(left.MaxPower, right.MaxPower);
+            var array = new double?[max];
+            for (int i = 0; i < max; i++)
+            {
+                array[i] = (left[i] ?? 0) + (right[i] ?? 0);
+            }
+            return new CustomPolynom(array);
+        }
+
+        public static CustomPolynom Substract(CustomPolynom left, CustomPolynom right)
+        {
+            int max = Math.Max(left.MaxPower, right.MaxPower);
+            var array = new double?[max];
+            for (int i = 0; i < max; i++)
+            {
+                array[i] = (left[i] ?? 0) - (right[i] ?? 0);
+            }
+            return new CustomPolynom(array);
+        }
+
+        public static CustomPolynom MultyplyNumber(CustomPolynom left, double right)
+        {
+            int max = left.MaxPower;
+            var array = new double?[max];
+            for (int i = 0; i < max; i++)
+            {
+                array[i] = (left[i] ?? 0) * right;
+            }
+            return new CustomPolynom(array);
+        }
+
+        public static CustomPolynom MultyplyNumber(double left, CustomPolynom right)
+        {
+            return MultyplyNumber(right, left);
+        }
+        #endregion
+
+        #region Properties
+        public int MaxPower { get; private set; }
+
+        public double? this[int index]
+        {
+            get
+            {
+                if ((index < MaxPower) && (index > -1))
+                    return polynom[index];
+                return null;
+            }
+        }
+        #endregion     
+
+        #region Object virtual methods override
+        public bool Equals(CustomPolynom other)
+        {
+            if (MaxPower == other.MaxPower)
+            {
+                int i = 0;
+                while ((i < MaxPower) && (this[i] == other[i]))
+                    i++;
+                if (i == MaxPower)
+                    return true;
+            }
+            return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) 
+                return false;
+            if (ReferenceEquals(this, obj)) 
+                return true;
+            return obj is CustomPolynom && Equals((CustomPolynom)obj);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < MaxPower; i++)
+            {
+                if (this[i].GetValueOrDefault() != 0)
+                    sb.Append(String.Format("{0:0.00}*x^{1}", this[i], i));
+            }
+            return base.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public object Clone()
+        {
+            return new CustomPolynom(polynom);
+        }
+        #endregion
+        
+        #region Operator overload
+        public static CustomPolynom operator +(CustomPolynom left, CustomPolynom right)
+        {
+            return Add(left, right);
+        }
+
+        public static CustomPolynom operator -(CustomPolynom left, CustomPolynom right)
+        {
+            return Substract(left, right);
+        }
+
+        public static CustomPolynom operator *(CustomPolynom left, int right)
+        {
+            return MultyplyNumber(left, right);
+        }
+
+        public static CustomPolynom operator *(int left, CustomPolynom right)
+        {
+            return MultyplyNumber(right, left);
+        }
+        
+        public static bool operator ==(CustomPolynom left, CustomPolynom right)
+        {
+            return left.Equals(right);
+        }
+         
+        public static bool operator !=(CustomPolynom left, CustomPolynom right)
+        {
+            return !(left == right);
+        }
+        #endregion 
+    }
+}
---
+++ PolynomTest/CustomPolynomTest.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Polynom;
+
+namespace PolynomTest
+{
+    [TestClass]
+    public class CustomPolynomTest
+    {
+        [TestMethod]
+        public void Polynom_Create()
+        {
+            var polynom = new CustomPolynom(1, 0, 2, 0, 5);
+
+            Assert.AreEqual(5, polynom.MaxPower);
+        }
+
+        [TestMethod]
+        public void Polynom_EqualsOtherFirst()
+        {
+            var array = new double?[] {1, 0, 2, 4, 5};
+            var first = new CustomPolynom(array);
+            var second = new CustomPolynom(array);
+
+            Assert.IsTrue(first.Equals(second));
+        }
+
+        [TestMethod]
+        public void Polynom_EqualsOtherSecond()
+        {
+            var array = new double?[] { 1, 0, 2, 4, 5 };
+            var first = new CustomPolynom(array);
+            var second = new CustomPolynom(array);
+
+            Assert.IsTrue(first == second);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void Polynom_EqualsOtherNullReference()
+        {
+            var array = new double?[] { 1, 0, 2, 4, 5 };
+            var first = new CustomPolynom(array);
+            CustomPolynom second = null;
+
+            Assert.IsTrue(first == second);
+        }
+
+        [TestMethod]
+        public void Polynom_NotEqualsOther()
+        {
+            var array = new double?[] { 1, 0, 2, 4, 5 };
+            var first = new CustomPolynom(array);
+            var second = new CustomPolynom(array);
+
+            Assert.IsFalse(first != second);
+        }
+
+        [TestMethod]
+        public void Polynom_Clone()
+        {
+            var array = new double?[] { 1, 0, 2, 4, 5 };
+            var first = new CustomPolynom(array);
+            var second = (CustomPolynom)first.Clone();
+
+            Assert.IsTrue(first.Equals(second));
+        }
+
+        [TestMethod]
+        public void Polynom_Addition()
+        {
+            var firstArray = new double?[] { 1, 0, 2, 4, 5 };
+            var secondArray = new double?[] { 3, 6, 1, 0, 10, -2 };
+            var resultArray = new double?[] { 4, 6, 3, 4, 15, -2 };
+            var first = new CustomPolynom(firstArray);
+            var second = new CustomPolynom(secondArray);
+            var result = new CustomPolynom(resultArray);
+
+            var summa = first + second;
+
+            Assert.IsTrue(summa.Equals(result));
+        }
+
+        [TestMethod]
+        public void Polynom_Subtraction()
+        {
+            var firstArray = new double?[] { 1, 0, 2, 4, 5 };
+            var secondArray = new double?[] { 3, 6, 1, 0, 10, -2 };
+            var resultArray = new double?[] { -2, -6, 1, 4, -5, 2 };
+            var first = new CustomPolynom(firstArray);
+            var second = new CustomPolynom(secondArray);
+            var result = new CustomPolynom(resultArray);
+
+            var difference = first - second;
+
+            Assert.IsTrue(difference.Equals(result));
+        }
+
+        [TestMethod]
+        public void Polynom_MultiplicationOnNumberFirst()
+        {
+            int number = 3;
+            var firstArray = new double?[] { 1, 0, 2, 4, 5 };
+            var resultArray = new double?[] { 3, 0, 6, 12, 15 };
+            var first = new CustomPolynom(firstArray);
+            var result = new CustomPolynom(resultArray);
+
+            var product = first * number;
+
+            Assert.IsTrue(product.Equals(result));
+        }
+
+        [TestMethod]
+        public void Polynom_MultiplicationOnNumberSecond()
+        {
+            int number = 3;
+            var firstArray = new double?[] { 1, 0, 2, 4, 5 };
+            var resultArray = new double?[] { 3, 0, 6, 12, 15 };
+            var first = new CustomPolynom(firstArray);
+            var result = new CustomPolynom(resultArray);
+
+            var product = number * first;
+
+            Assert.IsTrue(product.Equals(result));
+        }
+    }
+}
