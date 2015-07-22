a7069d1905cd890e3c18e6344ee7def497ef84fd
AlexanderChechet <Chechetsasha95@gmail.com>
2015-07-22 20:27:18 +0300

    Day3Commit2

--- Polynom/CustomPolynom.cs
+++ Polynom/CustomPolynom.cs
@@ -6,60 +6,73 @@ namespace Polynom
     public class CustomPolynom : IEquatable<CustomPolynom>, ICloneable
     {
         #region private members
-        private double?[] polynom;
+        private readonly double[] polynom;
+        private readonly double accuracy;
         #endregion
 
         #region Ctors
         public CustomPolynom(CustomPolynom customPolynom)
         {
+            accuracy = 0.0001;
             MaxPower = customPolynom.MaxPower;
-            polynom = new double?[MaxPower];
+            polynom = new double[MaxPower];
             for (int i = 0; i < MaxPower; i++)
             {
                 polynom[i] = customPolynom[i];
             }
         }
 
-        public CustomPolynom(params double?[] array)
+        public CustomPolynom(params double[] array)
         {
+            accuracy = 0.0001;
             if (array == null)
                 throw new ArgumentNullException();
             MaxPower = array.Length;
-            polynom = new double?[MaxPower];
+            polynom = new double[MaxPower];
             array.CopyTo(polynom, 0);
         }
         #endregion
-
+                                                                                               
         #region Static methods
         public static CustomPolynom Add(CustomPolynom left, CustomPolynom right)
         {
+            if (ReferenceEquals(null, left))
+                throw new ArgumentNullException();
+            if (ReferenceEquals(null, right))
+                throw new ArgumentNullException();
             int max = Math.Max(left.MaxPower, right.MaxPower);
-            var array = new double?[max];
+            var array = new double[max];
             for (int i = 0; i < max; i++)
             {
-                array[i] = (left[i] ?? 0) + (right[i] ?? 0);
+                array[i] = left[i] + right[i];
             }
             return new CustomPolynom(array);
         }
 
         public static CustomPolynom Substract(CustomPolynom left, CustomPolynom right)
         {
+            if (ReferenceEquals(null, left))
+                throw new ArgumentNullException();
+            if (ReferenceEquals(null, right))
+                throw new ArgumentNullException();
             int max = Math.Max(left.MaxPower, right.MaxPower);
-            var array = new double?[max];
+            var array = new double[max];
             for (int i = 0; i < max; i++)
             {
-                array[i] = (left[i] ?? 0) - (right[i] ?? 0);
+                array[i] = left[i] - right[i];
             }
             return new CustomPolynom(array);
         }
 
         public static CustomPolynom MultyplyNumber(CustomPolynom left, double right)
         {
+            if (ReferenceEquals(null, left))
+                throw new ArgumentNullException();
             int max = left.MaxPower;
-            var array = new double?[max];
+            var array = new double[max];
             for (int i = 0; i < max; i++)
             {
-                array[i] = (left[i] ?? 0) * right;
+                array[i] = left[i]* right;
             }
             return new CustomPolynom(array);
         }
@@ -68,29 +81,30 @@ namespace Polynom
         {
             return MultyplyNumber(right, left);
         }
-        #endregion
-
+        #endregion                                                        
+                      
         #region Properties
         public int MaxPower { get; private set; }
 
-        public double? this[int index]
+        public double this[int index]
         {
             get
             {
                 if ((index < MaxPower) && (index > -1))
                     return polynom[index];
-                return null;
+                throw new ArgumentException();
             }
         }
         #endregion     
 
+        //TO DO HERE
         #region Object virtual methods override
         public bool Equals(CustomPolynom other)
         {
             if (MaxPower == other.MaxPower)
             {
                 int i = 0;
-                while ((i < MaxPower) && (this[i] == other[i]))
+                while ((i < MaxPower) && (Math.Abs(this[i] - other[i]) < accuracy))
                     i++;
                 if (i == MaxPower)
                     return true;
@@ -112,7 +126,7 @@ namespace Polynom
             var sb = new StringBuilder();
             for (int i = 0; i < MaxPower; i++)
             {
-                if (this[i].GetValueOrDefault() != 0)
+                if (this[i] < accuracy)
                     sb.Append(String.Format("{0:0.00}*x^{1}", this[i], i));
             }
             return base.ToString();
@@ -120,14 +134,14 @@ namespace Polynom
 
         public override int GetHashCode()
         {
-            return base.GetHashCode();
+            return base.GetHashCode(); //TO DO HERE
         }
 
         public object Clone()
         {
             return new CustomPolynom(polynom);
         }
-        #endregion
+        #endregion 
         
         #region Operator overload
         public static CustomPolynom operator +(CustomPolynom left, CustomPolynom right)
@@ -152,6 +166,8 @@ namespace Polynom
         
         public static bool operator ==(CustomPolynom left, CustomPolynom right)
         {
+            if (ReferenceEquals(null, left))
+                throw new ArgumentNullException();
             return left.Equals(right);
         }
          
